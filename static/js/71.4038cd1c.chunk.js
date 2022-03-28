4c5f229b34c59ab3bda4e4768235ddbd400353bb
akshayDatacode <akshay.webllisto@gmail.com>
2022-03-28 18:04:55 +0530

    Updates

--- static/js/71.4038cd1c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_oms=self.webpackChunkreact_oms||[]).push([[71],{3071:function(e,n,r){r.r(n),r.d(n,{default:function(){return c}});r(2791);var t=r(184),c=function(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)("h1",{children:"Products"})})}}}]);
-//# sourceMappingURL=71.4038cd1c.chunk.js.map
